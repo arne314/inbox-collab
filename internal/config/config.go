88625788d37d5a966166fe8360e896966781f38d
arne314 <73391160+arne314@users.noreply.github.com>
2025-02-21 16:40:35 +0100

    fix: ignore `"` at start/end in `.env`

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,6 +25,15 @@ type Config struct {
 	DatabaseUrl      string
 }
 
+func (c *Config) getenv(name string) string {
+	val := os.Getenv(name)
+	runes := []rune(val)
+	if runes[0] == '"' && runes[len(runes)-1] == '"' {
+		return string(runes[1:(len(runes) - 1)])
+	}
+	return val
+}
+
 func (c *Config) Load() {
 	// load config.toml
 	file, err := os.ReadFile("config.toml")
@@ -43,14 +52,14 @@ func (c *Config) Load() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-	c.MatrixHomeServer = os.Getenv("MATRIX_HOMESERVER")
-	c.MatrixUsername = os.Getenv("MATRIX_USERNAME")
-	c.MatrixPassword = os.Getenv("MATRIX_PASSWORD")
-	c.DatabaseUrl = os.Getenv("DATABASE_URL")
+	c.MatrixHomeServer = c.getenv("MATRIX_HOMESERVER")
+	c.MatrixUsername = c.getenv("MATRIX_USERNAME")
+	c.MatrixPassword = c.getenv("MATRIX_PASSWORD")
+	c.DatabaseUrl = c.getenv("DATABASE_URL")
 
 	for name, mailConfig := range c.Mail {
-		mailConfig.Username = os.Getenv(fmt.Sprintf("MAIL_%s_USERNAME", strings.ToUpper(name)))
-		mailConfig.Password = os.Getenv(fmt.Sprintf("MAIL_%s_PASSWORD", strings.ToUpper(name)))
+		mailConfig.Username = c.getenv(fmt.Sprintf("MAIL_%s_USERNAME", strings.ToUpper(name)))
+		mailConfig.Password = c.getenv(fmt.Sprintf("MAIL_%s_PASSWORD", strings.ToUpper(name)))
 		if mailConfig.Username == "" || mailConfig.Password == "" {
 			log.Fatalf("Incomplete mail credentials provided for %v", name)
 		}
