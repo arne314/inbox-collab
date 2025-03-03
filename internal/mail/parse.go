36636b2b74518ea080eadd6e41f8d7d0fd0c9a2c
arne314 <73391160+arne314@users.noreply.github.com>
2025-03-03 19:21:50 +0100

    refactor: separate mail parsing from fetching

---
+++ internal/mail/parse.go
@@ -0,0 +1,99 @@
+package mail
+
+import (
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/emersion/go-imap/v2/imapclient"
+	"github.com/jhillyerd/enmime"
+	log "github.com/sirupsen/logrus"
+)
+
+func (mf *MailFetcher) parseHeaderRegex(
+	regex *regexp.Regexp,
+	header string,
+	allowEmpty bool,
+	lower bool,
+) []string {
+	parsed := regex.FindAllStringSubmatch(header, -1)
+	if !allowEmpty && len(parsed) == 0 {
+		return []string{""}
+	}
+	matches := make([]string, len(parsed))
+	for i, match := range parsed {
+		matches[i] = match[1]
+		if lower {
+			matches[i] = strings.ToLower(matches[i])
+		}
+	}
+	return matches
+}
+
+func (mf *MailFetcher) parseAddresses(header string, allowEmpty bool) []string {
+	return mf.parseHeaderRegex(mf.addressRegex, header, allowEmpty, true)
+}
+
+func (mf *MailFetcher) parseIds(header string, allowEmpty bool) []string {
+	return mf.parseHeaderRegex(mf.idRegex, header, allowEmpty, false)
+}
+
+func (mf *MailFetcher) parseNameFrom(header string) string {
+	matches := mf.nameFromRegex.FindStringSubmatch(header)
+	if matches != nil {
+		return matches[1]
+	}
+	return ""
+}
+
+func (mf *MailFetcher) parseMessage(msg *imapclient.FetchMessageData) *Mail {
+	var bodySection imapclient.FetchItemDataBodySection
+	ok := false
+	for {
+		item := msg.Next()
+		if item == nil {
+			break
+		}
+		bodySection, ok = item.(imapclient.FetchItemDataBodySection)
+		if ok {
+			break
+		}
+	}
+	if !ok {
+		log.Errorf("FETCH command for %v did not return body section", mf.name)
+		return nil
+	}
+
+	var envelope *enmime.Envelope
+	envelope, err := enmime.ReadEnvelope(bodySection.Literal)
+	if err != nil {
+		log.Errorf("Failed to parse mail for %v: %v", mf.name, err)
+		return nil
+	}
+	var date time.Time
+	if date, err = envelope.Date(); err != nil {
+		log.Errorf("Failed to parse date of mail for %v: %v", mf.name, err)
+		return nil
+	}
+	var attachments []string
+	for _, att := range envelope.Attachments {
+		attachments = append(attachments, att.FileName)
+	}
+	parsedMail := &Mail{
+		MessageId:   mf.parseIds(envelope.GetHeader("Message-ID"), false)[0],
+		InReplyTo:   mf.parseIds(envelope.GetHeader("In-Reply-To"), false)[0],
+		References:  mf.parseIds(envelope.GetHeader("References"), true),
+		NameFrom:    mf.parseNameFrom(envelope.GetHeader("From")),
+		AddrFrom:    mf.parseAddresses(envelope.GetHeader("From"), false)[0],
+		AddrTo:      mf.parseAddresses(envelope.GetHeader("To"), true),
+		Subject:     envelope.GetHeader("Subject"),
+		Date:        date,
+		Text:        envelope.Text,
+		Attachments: attachments,
+	}
+	if parsedMail.MessageId == "" || parsedMail.Text == "" {
+		log.Errorf("Skipping invalid mail for %v: %v", mf.name, parsedMail)
+		return nil
+	}
+	return parsedMail
+}
